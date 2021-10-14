1f007b4db27c3369ab753b1936acec41a0fdc541
su <su@209-6>
2021-10-14 14:36:38 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите время:");
+                double x = double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите время:");
+                double y = double.Parse(Console.ReadLine());
+                if (((y < 1) && (x > -1) && (y > x + 1)) || 
+                    ((y > -1) && (x > -1) && (y < -x - 1)) || 
+                    ((y > -1) && (x < 1) && (y < x - 1)) || 
+                    ((y < 1) && (x < 1) && (y > -x + 1))) 
+                    Console.WriteLine("ПОПАЛ");
+
+                else Console.WriteLine("НЕ ПОПАЛ");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
